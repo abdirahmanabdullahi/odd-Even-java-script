ffde8b5bb67af02fd6773a67502ec5451bdc8f90
abdirahmanabdullahi <33281290+abdirahmanabdullahi@users.noreply.github.com>
2022-05-15 14:39:00 +0300

    Add files via upload

---
+++ evenOdd-js.js
@@ -0,0 +1,14 @@
+function oddEven(){
+    var i = -1
+    while(i < 100){
+        i++
+        if (i % 2 == 0){
+            console.log("The number " + i + " is even.")
+        }else{
+            console.log("The number " + i + " is odd.")
+
+        }
+    }
+
+}
+oddEven()
